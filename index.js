094a837fa4cfc0c98e64a00b2f853f5d2b61c02f
maurasal <144288977+maurasal@users.noreply.github.com>
2023-11-12 01:18:13 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+const README =
+// TODO: Create a function to write README file
+function writeToFile(README, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
